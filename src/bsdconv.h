1d4e7e99949acd63d69e7b9bcdecf25702b2071d
Charlie <root@Zeta.twbbs.org>
2009-04-11 10:19:23 +0800

    revert the revert: describe pointer by integer again to avoid compile error

--- src/bsdconv.h
+++ src/bsdconv.h
@@ -5,12 +5,12 @@ enum bsdconv_status{
 };
 
 struct data_s{
-	unsigned char *data;
+	int data;
 	size_t len;
 };
 
 struct state_s{
 	int status;
-	unsigned char *data;
-	unsigned char *sub[257];
+	int data;
+	int sub[257];
 };
