89677822cf67d48e5b97af8b4bf4982a836d525d
Charlie <root@Zeta.twbbs.org>
2009-04-11 09:32:16 +0800

    change struct for pass one by one unichar, instead of byte stream

--- src/bsdconv.h
+++ src/bsdconv.h
@@ -4,9 +4,13 @@ enum bsdconv_status{
 	MATCH,	
 };
 
-struct state_s{
-	char status;
+struct data_s{
 	void *data;
 	size_t len;
-	struct state_s *sub[257];
+}
+
+struct state_s{
+	char status;
+	struct data_s *data;
+	int sub[257];
 };
