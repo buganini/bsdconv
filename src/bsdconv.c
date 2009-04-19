8feacdddeb0ffb235dacb713282c4860b49d3031
Buganini <buganini@gmail.com>
2009-04-19 22:11:49 +0800

    enhance commandline usage and exception handling

--- src/bsdconv.c
+++ src/bsdconv.c
@@ -5,21 +5,47 @@
 int main(int argc, char *argv[]){
 	struct bsdconv_instruction ins;
 	struct bsdconv_t *cd;
-	FILE *inf=fopen(argv[2],"rb");
+	FILE *inf, *otf;
 	unsigned char in[1024], out[1024];
-	cd=bsdconv_create(argv[1]);
-	bsdconv_init(cd, &ins, in, 1024, out, 1024);
+
 	int r;
-	if(!inf){
-		fprintf(stderr, "Unable to open file %s\n", argv[2]);
+	if(argc<2){
+		fprintf(stderr, "Usage:\n\t %s from:inter:to [input] [output]\n", argv[0]);
 		exit(1);
 	}
+	if(argc>2){
+		inf=fopen(argv[2],"r");
+		if(!inf){
+			fprintf(stderr, "Unable to open input file %s\n", argv[2]);
+			exit(1);
+		}
+	}else{
+		inf=stdin;
+	}
+	if(argc>3){
+		otf=fopen(argv[3],"w");
+		if(!otf){
+			fprintf(stderr, "Unable to open output file %s\n", argv[3]);
+			exit(1);
+		}
+	}else{
+		otf=stdout;
+	}
+
+	cd=bsdconv_create(argv[1]);
+	bsdconv_init(cd, &ins, in, 1024, out, 1024);
 	do{
 		if(ins.feed_len) ins.feed_len=fread(ins.feed, 1, ins.feed_len, inf);
 		r=bsd_conv(cd, &ins);
-		if(ins.back_len)fwrite(ins.back, 1, ins.back_len, stdout);
+		if(ins.back_len)fwrite(ins.back, 1, ins.back_len, otf);
 	}while(r);
 	bsdconv_destroy(cd);
+	if(inf!=stdin){
+		fclose(inf);
+	}
+	if(otf!=stdout){
+		fclose(otf);
+	}
 	return 0;
 }
 
