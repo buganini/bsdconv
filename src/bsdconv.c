84ea5572a8edfa3f2763645eb3dd9d38ef18dd05
Buganini <buganini@gmail.com>
2009-04-21 22:32:00 +0800

    always display i/o statistics, in stderr

--- src/bsdconv.c
+++ src/bsdconv.c
@@ -6,34 +6,38 @@ int main(int argc, char *argv[]){
 	struct bsdconv_instruction ins;
 	struct bsdconv_t *cd;
 	FILE *inf, *otf;
-	unsigned char in[1024], out[1024], verbose=0;
+	unsigned char in[1024], out[1024];
 
 	int r;
 	if(argc<2){
-		fprintf(stderr, "Usage:\n\t %s from[,from2...]:[inter[,inter2,...]]:to[,to2...] [input [output]]\n", argv[0]);
+		fprintf(stderr, "Usage:\n\t %s from[,from2...]:[inter[,inter2,...]]:to[,to2...] [input|- [output|-]]\n", argv[0]);
 		exit(1);
 	}
 	if(argc>2){
-		inf=fopen(argv[2],"r");
-		if(!inf){
-			fprintf(stderr, "Unable to open input file %s\n", argv[2]);
-			exit(1);
+		if(strcmp(argv[2],"-")==0){
+			inf=stdin;
+		}else{
+			inf=fopen(argv[2],"r");
+			if(!inf){
+				fprintf(stderr, "Unable to open input file %s\n", argv[2]);
+				exit(1);
+			}
 		}
 	}else{
 		inf=stdin;
 	}
 	if(argc>3){
-		otf=fopen(argv[3],"w");
-		if(!otf){
-			fprintf(stderr, "Unable to open output file %s\n", argv[3]);
-			exit(1);
+		if(strcmp(argv[3],"-")==0){
+			otf=stdout;
+		}else{
+			otf=fopen(argv[3],"w");
+			if(!otf){
+				fprintf(stderr, "Unable to open output file %s\n", argv[3]);
+				exit(1);
+			}
 		}
 	}else{
 		otf=stdout;
-	}
-
-	if(argc>4 && strcmp(argv[4],"-v")==0){
-		verbose=1;
 	}
 
 	cd=bsdconv_create(argv[1]);
@@ -44,10 +48,8 @@ int main(int argc, char *argv[]){
 		if(ins.back_len)fwrite(ins.back, 1, ins.back_len, otf);
 	}while(r);
 
-	if(verbose){
-		fprintf(stderr, "Input failure: %u\n", ins.ierr);
-		fprintf(stderr, "Output failure: %u\n", ins.oerr);
-	}
+	fprintf(stderr, "Input failure: %u\n", ins.ierr);
+	fprintf(stderr, "Output failure: %u\n", ins.oerr);
 
 	bsdconv_destroy(cd);
 	if(inf!=stdin){
