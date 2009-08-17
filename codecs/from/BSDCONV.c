dd7efe3731a4cdb0ee437d8701841b956fd9178f
Buganini <buganini@gmail.com>
2009-08-18 00:49:28 +0800

    make it actually works

--- codecs/from/BSDCONV.c
+++ codecs/from/BSDCONV.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include "../../src/bsdconv.h"
 
+#define F_CLEAR 0
+#define F_A 1
+#define F_B 2
+
 struct my_s {
 	struct data_s data;
 	/* extend struct data_s */
@@ -19,7 +23,7 @@ void cbinit(struct bsdconv_codec_t *cdc, struct my_s *t){
 	t->data.data=NULL;
 	t->data.next=0;
 	t->size=0;
-	t->flag=0;
+	t->flag=F_A;
 
 }
 
@@ -34,19 +38,29 @@ void callback(struct bsdconv_instance *ins){
 	unsigned char d=*ins->from_data;
 	if(hex[d]==-1){
 		ins->from_state.status=DEADEND;
-		t->flag=1;
+		t->flag=F_CLEAR;
 	}else{
-		if(t->flag){
-			t->flag=0;
+		if(t->flag==F_CLEAR){
+			t->flag=F_A;
 			t->data.len=0;
 		}
 		ins->from_state.status=SUBMATCH;
 		ins->from_state.data=&(t->data);
-		if(t->data.len >= t->size){
-			t->size+=8;
-			t->data.data=realloc(t->data.data,t->size);
+		switch(t->flag){
+			case F_A:
+				if(t->data.len >= t->size){
+					t->size+=8;
+					t->data.data=realloc(t->data.data,t->size);
+				}
+				t->data.data[t->data.len]=hex[d];
+				t->data.len+=1;
+				t->flag=F_B;
+				break;
+			case F_B:
+				t->data.data[t->data.len-1]<<=4;
+				t->data.data[t->data.len-1]|=hex[d];
+				t->flag=F_A;
+				break;
 		}
-		t->data.data[t->data.len]=d;
-		t->data.len+=1;
 	}
 }
