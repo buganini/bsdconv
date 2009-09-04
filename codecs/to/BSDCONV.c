46294400d926853f45b10cd2c74c8eb6ed4cc6d8
Buganini <buganini@gmail.com>
2009-09-05 02:25:03 +0800

    fix to/BSDCONV

--- codecs/to/BSDCONV.c
+++ codecs/to/BSDCONV.c
@@ -9,14 +9,14 @@ void callback(struct bsdconv_instance *ins){
 	char *p;
 	ins->phase[ins->phasen].state.status=NEXTPHASE;
 
-	ins->phase[ins->phasen-1].data_tail->next=malloc(sizeof(struct data_s));
-	ins->phase[ins->phasen-1].data_tail=ins->phase[ins->phasen-1].data_tail->next;
-	ins->phase[ins->phasen-1].data_tail->next=NULL;
+	ins->phase[ins->phasen].data_tail->next=malloc(sizeof(struct data_s));
+	ins->phase[ins->phasen].data_tail=ins->phase[ins->phasen].data_tail->next;
+	ins->phase[ins->phasen].data_tail->next=NULL;
 
-	ins->phase[ins->phasen-1].data_tail->len=ins->phase[ins->phasen].data->len*2;;
-	p=ins->phase[ins->phasen-1].data_tail->data=malloc(ins->phase[ins->phasen-1].data_tail->len);
+	ins->phase[ins->phasen].data_tail->len=ins->phase[ins->phasen-1].data->len*2;;
+	p=ins->phase[ins->phasen].data_tail->data=malloc(ins->phase[ins->phasen].data_tail->len);
 	for(i=0;i<ins->phase[ins->phasen-1].data_tail->len;++i){
-		sprintf(p,"%02X",ins->phase[ins->phasen].data->data[i]);
+		sprintf(p,"%02X",ins->phase[ins->phasen-1].data->data[i]);
 		TAILIZE(p);
 	}
 }
