4008a1224bf2c9bc53bc6539748e166bb573e3f3
Buganini <buganini@gmail.com>
2009-09-17 00:48:04 +0800

    add to/UCS-2BE and to/UCS2-LE

---
+++ codecs/to/UCS-2LE.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "../../src/bsdconv.h"
+
+#define SWAP(a,b,i) ((i)=(a), (a)=(b), (b)=(i))
+
+void callback(struct bsdconv_instance *ins){
+	unsigned char *data;
+	unsigned int len, i;
+	struct bsdconv_phase *this_phase=&ins->phase[ins->phasen];
+	struct bsdconv_phase *prev_phase=&ins->phase[ins->phasen-1];
+	data=prev_phase->data->data;
+	if(*data!=0x01 || prev_phase->data->len > 3){
+		this_phase->state.status=DEADEND;
+		return;
+	}
+	this_phase->state.status=NEXTPHASE;
+	data+=1;
+	len=prev_phase->data->len-1;
+
+	this_phase->data_tail->next=malloc(sizeof(struct data_s));
+	this_phase->data_tail=this_phase->data_tail->next;
+	this_phase->data_tail->next=NULL;
+	this_phase->data_tail->len=2;
+	this_phase->data_tail->data=malloc(2);
+	for(i=0;i<2-len;++i){
+		this_phase->data_tail->data[i]=0x0;
+	}
+	memcpy(&this_phase->data_tail->data[i], data, len);
+	data=this_phase->data_tail->data;
+
+	SWAP(data[0],data[1],i);
+	return;
+}
