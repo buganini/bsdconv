6cd8e173ac5213329fc56618bdba6f9af42d7678
Buganini <buganini@gmail.com>
2009-10-10 03:37:22 +0800

    use setup.py instead of Makefile

--- wrapper/python/Makefile
+++
@@ -1,2 +0,0 @@
-all:
-	$(CC) bsdconv.c -Wall -L/usr/local/lib -I/usr/local/include -I/usr/local/include/python2.6/ -fPIC -shared -lbsdconv -o bsdconv.so
---
+++ wrapper/python/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup,Extension
+
+setup(name='bsdconv',
+      version='3.5',
+      description='bsdconv library',
+      author='Buganini',
+      author_email='buganini@gmail.com',
+      url='http://github.com/buganini/bsdconv',
+      ext_modules=[Extension('bsdconv', ['bsdconv.c'],include_dirs=['/usr/local/include'],libraries=['bsdconv'],library_dirs=['/usr/local/lib'])],
+     )
+
