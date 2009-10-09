148c9a3bbd3e7fd359c399c9e993037c1f47a9e6
Buganini <buganini@gmail.com>
2009-10-10 02:03:37 +0800

    start work on python wrapper, not usable yet

---
+++ wrapper/python/Makefile
@@ -0,0 +1,2 @@
+all:
+	$(CC) bsdconv.c -L/usr/local/lib -I/usr/local/include -I/usr/local/include/python2.6/ -fPIC -shared -lbsdconv -o Pybsdconv.so
---
+++ wrapper/python/bsdconv.c
@@ -0,0 +1,86 @@
+#include <Python.h>
+#include <bsdconv.h>
+
+PyDoc_STRVAR(bsdconv_create_doc,
+"create(c)\n\
+\n\
+Create bsdconv instance.");
+
+static PyObject *
+py_bsdconv_create(PyObject *self, PyObject *args)
+{
+	char *c;
+	struct bsdconv_instance *r;
+
+	if (!PyArg_ParseTuple(args, "s", &c))
+		return NULL;
+	r=bsdconv_create(c);
+	if(r==NULL){
+		Py_INCREF(Py_None);
+		return Py_None;
+	}
+	return (PyObject*)r;
+}
+
+PyDoc_STRVAR(bsdconv_destroy_doc,
+"destroy(p)\n\
+\n\
+Destroy bsdconv instance.");
+
+static PyObject *
+py_bsdconv_destroy(PyObject *self, PyObject *args)
+{
+	struct bsdconv_instance *r;
+	if (!PyArg_ParseTuple(args, "o", &r))
+		return NULL;
+	bsdconv_destroy(r);
+	return NULL;
+}
+
+PyDoc_STRVAR(bsdconv_conv_doc,
+"conv(p,s)\n\
+\n\
+Perform conversion.");
+
+static PyObject *
+py_bsdconv_conv(PyObject *self, PyObject *args)
+{
+	static PyObject *r;
+	struct bsdconv_instance *p;
+	char *s;
+	int l;
+	if (!PyArg_ParseTuple(args, "oz#", &p,&s,&l))
+		return NULL;
+	p->mode=BSDCONV_CC;
+	p->feed=s;
+	p->feed_len=l;
+	bsdconv_init(p);
+	bsdconv(p);
+	r=Py_BuildValue("z#",p->back, p->back_len);
+	free(p->back);
+	return r;
+}
+
+static PyMethodDef bsdconv_methods[] = {
+	{"create",	py_bsdconv_create,	METH_VARARGS,
+		PyDoc_STR("create() -> Create bsdconv instance")},
+	{"destroy",	py_bsdconv_destroy,	METH_VARARGS,
+		PyDoc_STR("destroy() -> Destroy bsdconv instance")},
+	{"conv",	py_bsdconv_conv,	METH_VARARGS,
+		PyDoc_STR("conv() -> Perform conversion")},
+	{NULL,		NULL}		/* sentinel */
+};
+
+PyDoc_STRVAR(module_doc,
+"BSD licensed charset/encoding converter library");
+
+/* Initialization function for the module (*must* be called initxx) */
+
+PyMODINIT_FUNC
+initbsdconv(void)
+{
+	PyObject *m;
+	m = Py_InitModule3("bsdconv", bsdconv_methods, module_doc);
+	if (m == NULL)
+		return;
+}
