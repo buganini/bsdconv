987589c0ceb213f626b041fd4b3d5ad52c88d25b
Buganini <buganini@gmail.com>
2009-10-11 16:33:08 +0800

    improve irssi script

--- addons/irssiScript/bsdconv.pl
+++ addons/irssiScript/bsdconv.pl
@@ -15,13 +15,57 @@ $VERSION = '2009060600';
 
 my $bsdconv_ignore = 0;
 
-sub event_send_text () {
+sub on_recv () {
+	if($bsdconv_ignore){
+		return;
+	}
+	my ($server, $line, $nick, $address) = @_;
+	my $conversion = Irssi::settings_get_str('bsdconv_in');
+	$conversion =~ s/^\s+|\s+$//g;
+	my $h = bsdconv::create($conversion);
+	if($conversion=='') return;
+	if(!defined($h)){
+		Irssi:print(bsdconv::error());
+		return;
+	}
+	my $line=bsdconv::conv($h,$line);
+	bsdconv::destroy($h);
+	$bsdconv_ignore = 1;
+	Irssi::signal_emit('event privmsg', ($server, $line, $nick, $address));
+	$bsdconv_ignore = 0;
+	Irssi::signal_stop();
+}
+
+sub on_topic () {
+	if($bsdconv_ignore){
+		return;
+	}
+	my ($server_rec) = @_;
+	my $conversion = Irssi::settings_get_str('bsdconv_in');
+	$conversion =~ s/^\s+|\s+$//g;
+	if($conversion=='') return;
+	my $h = bsdconv::create($conversion);
+	if(!defined($h)){
+		Irssi:print(bsdconv::error());
+		return;
+	}
+	my $line=bsdconv::conv($h,$server_rec->{topic});
+	$server_rec->{topic}=$line;
+	bsdconv::destroy($h);
+	$bsdconv_ignore = 1;
+	Irssi::signal_emit('channel topic changed', $server_rec);
+	$bsdconv_ignore = 0;
+	Irssi::signal_stop();
+}
+
+sub bsdconv_out () {
 	if($bsdconv_ignore){
 		return;
 	}
 	my ($line, $server_rec, $wi_item_rec) = @_;
-	my $conversion = Irssi::settings_get_str('bsdconv');
+	my $conversion = Irssi::settings_get_str('bsdconv_out');
 	$conversion =~ s/^\s+|\s+$//g;
+	if($conversion=='') return;
 	my $h = bsdconv::create($conversion);
 	if(!defined($h)){
 		Irssi:print(bsdconv::error());
@@ -31,9 +75,12 @@ sub event_send_text () {
 	bsdconv::destroy($h);
 	$bsdconv_ignore = 1;
 	Irssi::signal_emit('send text', $line,  $server_rec, $wi_item_rec);
-	Irssi::signal_stop();
 	$bsdconv_ignore = 0;
+	Irssi::signal_stop();
 }
 
-Irssi::signal_add_first('send text', "event_send_text");
-Irssi::settings_add_str("misc", "bsdconv", "utf-8:utf-8");
+Irssi::signal_add('event privmsg', "on_recv");
+#Irssi::signal_add('channel topic changed', "on_topic");
+Irssi::signal_add_first('send text', "bsdconv_out");
+Irssi::settings_add_str("misc", "bsdconv_in", "utf-8,ascii:utf-8,ascii");
+Irssi::settings_add_str("misc", "bsdconv_out", "utf-8,ascii:utf-8,ascii");
