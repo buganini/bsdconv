1134dc09acac617f9b2a69e4ef11468e83270c9f
Buganini <buganini@gmail.com>
2009-10-11 19:23:24 +0800

    bump version

--- wrapper/perl/lib/bsdconv.pm
+++ wrapper/perl/lib/bsdconv.pm
@@ -25,7 +25,7 @@ our @EXPORT = qw(
 	
 );
 
-our $VERSION = '3.3';
+our $VERSION = '3.5';
 
 require XSLoader;
 XSLoader::load('bsdconv', $VERSION);
--- wrapper/php/bsdconv.c
+++ wrapper/php/bsdconv.c
@@ -176,7 +176,7 @@ zend_module_entry bsdconv_module_entry = {
 	NULL,
 	PHP_MINFO(bsdconv),
 #if ZEND_MODULE_API_NO >= 20010901
-	"3.3", /* Replace with version number for your extension */
+	"3.5", /* Replace with version number for your extension */
 #endif
 	STANDARD_MODULE_PROPERTIES
 };
