835294099fc18fd677a04f795858adf1adfe9b13
Buganini <buganini@gmail.com>
2010-01-26 09:14:44 +0800

    give a space to trailing '\0'

--- codecs/to/BSDCONV.c
+++ codecs/to/BSDCONV.c
@@ -32,7 +32,7 @@ void callback(struct bsdconv_instance *ins){
 	this_phase->data_tail=this_phase->data_tail->next;
 	this_phase->data_tail->next=NULL;
 
-	this_phase->data_tail->len=prev_phase->data->len*2;;
+	this_phase->data_tail->len=prev_phase->data->len*2+1;
 	p=this_phase->data_tail->data=malloc(this_phase->data_tail->len);
 	for(i=0;i<prev_phase->data->len;++i){
 		sprintf(p,"%02X",prev_phase->data->data[i]);
