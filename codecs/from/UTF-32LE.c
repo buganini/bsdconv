84f5a879e5a038e5781f28fbba7782548edf4a70
Buganini Q <buganini@gmail.com>
2011-01-04 01:02:10 +0800

    happy new year

--- codecs/from/UTF-32LE.c
+++ codecs/from/UTF-32LE.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009,2010 Kuan-Chung Chiu <buganini@gmail.com>
+ * Copyright (c) 2009-2011 Kuan-Chung Chiu <buganini@gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
