e0cc2c87ba89714d182ad3ce5204be105a668747
Buganini Q <buganini@gmail.com>
2011-01-05 19:21:53 +0800

    chase previous commit

--- codecs/to/UNICODE.c
+++ codecs/to/UNICODE.c
@@ -3,18 +3,24 @@
 void callback(struct bsdconv_instance *ins){
 	struct bsdconv_phase *this_phase=&ins->phase[ins->phase_index];
 	struct bsdconv_phase *prev_phase=&ins->phase[ins->phase_index-1];
+	struct data_rt *data_ptr;
 
 	this_phase->bak=this_phase->data->next;
-	while(prev_phase->data_head->next!=this_phase->bak){
-		this_phase->data_tail->next=prev_phase->data_head->next;
-		if(prev_phase->data_tail==prev_phase->data_head->next){
-			prev_phase->data_tail=prev_phase->data_head;
-		}
-		prev_phase->data_head->next=prev_phase->data_head->next->next;
-		this_phase->data_tail=this_phase->data_tail->next;
-		this_phase->data_tail->flags |= F_SKIP;
-		this_phase->data_tail->next=NULL;
+	while(prev_phase->data_head->next!=this_phase->data){
+		data_ptr=prev_phase->data_head->next->next;
+		DATA_FREE(prev_phase->data_head->next);
+		prev_phase->data_head->next=data_ptr;
 	}
+	this_phase->data_tail->next=prev_phase->data_head->next;
+	if(prev_phase->data_tail==prev_phase->data_head->next){
+		prev_phase->data_tail=prev_phase->data_head;
+	}
+	prev_phase->data_head->next=prev_phase->data_head->next->next;
+	this_phase->data_tail=this_phase->data_tail->next;
+	this_phase->data_tail->flags |= F_SKIP;
+	this_phase->data_tail->next=NULL;
+
+	this_phase->data_head->len=0;
 	this_phase->data=prev_phase->data_head;
 	ins->phase[ins->phase_index].state.status=PASSTHRU;
 
