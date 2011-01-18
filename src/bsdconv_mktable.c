084a363caf2333b461b675567125d6c584cb4299
Buganini Q <buganini@gmail.com>
2011-01-18 19:28:50 +0800

    bsdconv_mktable dedup for data and data list

--- src/bsdconv_mktable.c
+++ src/bsdconv_mktable.c
@@ -39,7 +39,6 @@ struct m_data_st{
 	struct data_st *next;
 
 	uintptr_t p;
-	char *dp;
 	struct m_data_st *n;
 };
 
@@ -63,20 +62,74 @@ struct list{
 	struct list *n;
 };
 
+struct dhash{
+	uintptr_t v;
+	int c;
+	struct dhash *p;
+	struct dhash *sub[257];
+	uintptr_t offset;
+	uintptr_t head;
+};
+
 char table[256]={};
 char ci_table[256]={0};
 
 uintptr_t offset=0;
 
+struct dhash *hash_datalist, *hash_data;
+
+uintptr_t hash(int *p, uintptr_t l){
+	int i,j;
+	struct dhash *hash_p=hash_datalist;
+	struct dhash *hash_q=hash_data;
+	for(i=l-1;i>=0;--i){
+		if(hash_p->sub[p[i]]==NULL){
+			hash_p->sub[p[i]]=malloc(sizeof(struct dhash));
+			hash_p->sub[p[i]]->c=p[i];
+			hash_p->sub[p[i]]->p=hash_p;
+			hash_p->sub[p[i]]->v=0;
+			hash_p->sub[p[i]]->offset=0;
+			for(j=0;j<=256;++j){
+				hash_p->sub[p[i]]->sub[j]=NULL;
+			}
+		}
+//		printf("hash_p: [%d]%X %p => %p\n", i, p[i], hash_p, hash_p->sub[p[i]]);
+		if(p[i]==256){
+			hash_p->v=(uintptr_t) hash_q;
+			hash_p=hash_p->sub[p[i]];
+			hash_q=hash_data;
+		}else{
+			hash_p=hash_p->sub[p[i]];
+			if(hash_q->sub[p[i]]==NULL){
+				hash_q->sub[p[i]]=malloc(sizeof(struct dhash));
+				hash_q->sub[p[i]]->c=p[i];
+				hash_q->sub[p[i]]->p=hash_q;
+				hash_q->sub[p[i]]->v=0;
+				hash_q->sub[p[i]]->offset=0;
+				hash_q->sub[p[i]]->head=0;
+				for(j=0;j<=256;++j){
+					hash_q->sub[p[i]]->sub[j]=NULL;
+				}
+			}
+			hash_q=hash_q->sub[p[i]];
+		}
+	}
+	hash_p->v=(uintptr_t) hash_q;
+	return (uintptr_t) hash_p;
+}
+
 int main(int argc, char *argv[]){
-	int i, j, k, l, c=0, cu, cl, ci,pr;
+	int i, j, k, c=0, cu, cl, ci,pr;
 	FILE *fp;
-	char inbuf[1024], *f, *t, dat[256], *tmp, *of, *ot;
+	char inbuf[1024], *f, *t, *tmp, *of, *ot;
+	int dat[1024];
+	uintptr_t l,ret;
 	struct m_data_st *data_r=NULL, *data_p=NULL, *data_t=NULL;
 	struct m_state_st *state_r, *state_t, holder;
 	struct list *todo=NULL, *newtodo, *newtodo_tail, *state_p;
 	struct state_st dstate;
 	struct data_st ddata;
+	struct dhash *hash_p;
 	uintptr_t callback=0;
 	void *tofree;
 
@@ -158,6 +211,24 @@ int main(int argc, char *argv[]){
 	printf("Making table %s\n", argv[1]);
 
 	fp=fopen(argv[1], "r");
+
+	hash_datalist=malloc(sizeof(struct dhash));
+	for(i=0;i<=256;++i){
+		hash_datalist->sub[i]=0;
+	}
+	hash_datalist->p=0;
+	hash_datalist->v=0;
+	hash_datalist->offset=0;
+	hash_datalist->head=0;
+
+	hash_data=malloc(sizeof(struct dhash));
+	for(i=0;i<=256;++i){
+		hash_data->sub[i]=0;
+	}
+	hash_data->p=0;
+	hash_data->v=0;
+	hash_data->offset=0;
+	hash_data->head=0;
 
 	newtodo=malloc(sizeof(struct list));
 	newtodo->n=NULL;
@@ -270,7 +341,7 @@ int main(int argc, char *argv[]){
 						newtodo_tail->p->status=CONTINUE;
 						newtodo_tail->p->data=0;
 						newtodo_tail->p->child=0;
-						for(i=0;i<257;i++){
+						for(i=0;i<=256;i++){
 							newtodo_tail->p->sub[i]=0;
 							newtodo_tail->p->psub[i]=NULL;
 						}
@@ -321,44 +392,13 @@ int main(int argc, char *argv[]){
 				free(state_p);
 			}
 		}else while(1){
-			if(*t==',' || *t==0){
-				if(l){
-					if(data_p){
-						//data after head
-						//make new cell
-						data_t->n=(struct m_data_st *)malloc(sizeof(struct m_data_st));
-						data_p->next=(struct data_st *)offset;
-						data_p=data_t=data_t->n;
-					}else if(data_t){
-						//data head
-						//make new cell
-						data_t->n=(struct m_data_st *)malloc(sizeof(struct m_data_st));
-						data_p=data_t=data_t->n;
-					}else{
-						//frist
-						//make new cell
-						data_t=data_p=data_r=(struct m_data_st *)malloc(sizeof(struct m_data_st));
-					}
-
-					//init new cell
-					data_p->p=offset;
-					data_p->next=0;
-					data_p->n=NULL;
-					offset+=sizeof(struct data_st);
-
-					//put data
-					data_p->dp=malloc(l);
-					memcpy(data_p->dp, dat, l);
-					data_p->len=l;
-					data_p->data=(char *)offset;
-					offset+=l;
-				}
-
+			if(*t==0){
+				ret=hash(dat,l);
 				if(k){
 					k=0;
 					while(todo){
 						state_p=todo;
-						for(c=0;c<256;c++){
+						for(c=0;c<=256;c++){
 							if(c>=state_p->l && c<=state_p->u){
 								pr=1;
 							}else if(ci && ci_table[c] && ci_table[c]>=state_p->l && ci_table[c]<=state_p->u){
@@ -388,7 +428,7 @@ int main(int argc, char *argv[]){
 								offset+=sizeof(struct state_st);
 							}
 							if(l){
-								state_p->p->psub[c]->data=data_p->p;
+								state_p->p->psub[c]->data=ret;
 							}else{
 								state_p->p->psub[c]->data=0;
 							}
@@ -403,17 +443,18 @@ int main(int argc, char *argv[]){
 					data_p=NULL;
 					break;
 				}
+			}else if(*t==','){
+				dat[l]=256;
+				++l;
+			}else if(j==0){
+				c=table[(unsigned char)*t];
+				j=1;
 			}else{
-				if(j==0){
-					c=table[(unsigned char)*t];
-					j=1;
-				}else{
-					c*=16;
-					c+=table[(unsigned char)*t];
-					j=0;
-					dat[l]=c;
-					++l;
-				}
+				c*=16;
+				c+=table[(unsigned char)*t];
+				j=0;
+				dat[l]=c;
+				++l;
 			}
 			++t;
 		}
@@ -421,33 +462,121 @@ int main(int argc, char *argv[]){
 	}
 	fclose(fp);
 	fopen(argv[2], "wb+");
-	printf("Total size: %u\n", (unsigned int)offset);
 	state_t=state_r;
 	while(state_t){
 		dstate.status=state_t->status;
-		dstate.data=(struct data_st *)state_t->data;
+		hash_p=(struct dhash *)state_t->data;
+		k=1;	//begin of data cell
+		l=0;	//length counter
+		j=0;	//has read something to proceed
+		while(hash_p && hash_p->p){
+			if(k){
+				k=0;
+				if(!hash_p->offset){
+					j=1;
+					if(data_p){
+						//data after head
+						//make new cell
+						data_t->n=(struct m_data_st *)malloc(sizeof(struct m_data_st));
+						data_p=data_t=data_t->n;
+					}else if(data_t){
+						//data head
+						//make new cell
+						data_t->n=(struct m_data_st *)malloc(sizeof(struct m_data_st));
+						data_p=data_t=data_t->n;
+					}else{
+						//frist
+						//make new cell
+						data_t=data_p=data_r=(struct m_data_st *)malloc(sizeof(struct m_data_st));
+					}
+
+					//init new cell
+					hash_p->offset=data_p->p=offset;
+					data_p->next=0;
+					data_p->n=NULL;
+					offset+=sizeof(struct data_st);
+
+					data_p->data=(char *)hash_p->v;
+				}
+			}
+			if(hash_p->c==256){
+				k=1;
+				if(j){
+					j=0;
+					data_p->len=l;
+					data_p->next=(struct data_st *)offset;
+				}
+				l=0;
+			}else{
+				l+=1;				
+			}
+			hash_p=hash_p->p;
+		}
+		if(j){
+			j=0;
+			data_p->len=l;
+			data_p->next=0;
+		}
+		
+		hash_p=(struct dhash *)state_t->data;
+		if(hash_p)
+			dstate.data=(struct data_st *)hash_p->offset;
+		else
+			dstate.data=NULL;
 		for(i=0;i<257;i++){
 			dstate.sub[i]=state_t->sub[i];
 		}
 		fseek(fp, state_t->p, SEEK_SET);
+		//printf("Writing struct state_st.\n");
 		fwrite((void *)&dstate, sizeof(struct state_st), 1, fp);
 		tofree=state_t;
 		state_t=state_t->n;
 		free(tofree);
 	}
+
 	data_t=data_r;
 	while(data_t){
-		ddata.data=data_t->data;
+		hash_p=(struct dhash *)data_t->data;
+		if(hash_p && !hash_p->head){
+			hash_p->head=(uintptr_t)data_t->data;
+			hash_p=hash_p->p;
+			while(hash_p && hash_p->p){
+				hash_p->head=(uintptr_t)data_t->data;
+				hash_p=hash_p->p;
+			}
+		}
+		data_t=data_t->n;
+	}
+	
+	data_t=data_r;
+	while(data_t){
+		hash_p=(struct dhash *)data_t->data;
+		if(hash_p && !hash_p->offset){
+			fseek(fp, offset, SEEK_SET);
+			hash_p=(struct dhash *)hash_p->head;
+			while(hash_p && hash_p->p){
+				hash_p->offset=offset;
+				//printf("Writing byte: %X.\n", hash_p->c);
+				fputc(hash_p->c, fp);
+				offset+=1;
+				hash_p=hash_p->p;
+			}
+		}
+		hash_p=(struct dhash *)data_t->data;
+		if(hash_p)
+			ddata.data=(char *)hash_p->offset;
+		else
+			ddata.data=NULL;
 		ddata.len=data_t->len;
 		ddata.next=data_t->next;
 		fseek(fp, data_t->p, SEEK_SET);
+		//printf("Writing struct data_st.\n");
 		fwrite((void *)&ddata, sizeof(struct data_st), 1, fp);
-		fseek(fp, (uintptr_t)ddata.data, SEEK_SET);
-		fwrite((void *)data_t->dp, ddata.len, 1, fp);
 		tofree=data_t;
 		data_t=data_t->n;
 		free(tofree);
 	}
 	fclose(fp);
+	printf("Total size: %u\n", (unsigned int)offset);
 	return 0;
 }
