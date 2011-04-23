4f17668d8f0144ac1bec81ec976dcf89dca61915
Buganini Q <buganini@gmail.com>
2011-04-23 21:53:46 +0800

    rename from/unicode to from/skip and make it skip for all packet tagged F_SKIP

--- codecs/from/SKIP.c
+++ codecs/from/SKIP.c
@@ -3,7 +3,7 @@
 void callback(struct bsdconv_instance *ins){
 	struct bsdconv_phase *this_phase=&ins->phase[ins->phase_index];
 
-	if((this_phase->data->flags & F_SKIP) && this_phase->data->len>1 && CP(this_phase->data->data)[0]==0x01){
+	if((this_phase->data->flags & F_SKIP)){
 		this_phase->data->flags &= ~F_SKIP;
 		ins->phase[ins->phase_index].state.status=PASSTHRU;
 	}else{
