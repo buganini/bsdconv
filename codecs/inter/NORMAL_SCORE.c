d350525a8ddef1f850e48e9026b741502c0b046b
Buganini Q <buganini@gmail.com>
2011-04-28 17:21:16 +0800

    improve score table

--- codecs/inter/NORMAL_SCORE.c
+++ codecs/inter/NORMAL_SCORE.c
@@ -26,15 +26,21 @@ struct interval {
 };
 
 static const struct interval scoreboard[] = {
-	{ 0x0, 0x7f, 5 },
-	{ 0x3400, 0x4DB5, 3 },
-	{ 0x4E00, 0x9FA5, 4 },
-	{ 0x9FA6, 0x9FBB, 3 },
-	{ 0xF900, 0xFA2D, 4 },
-	{ 0xFA30, 0xFA6A, 4 },
-	{ 0xFA70, 0xFAD9, 2 },
-	{ 0x20000, 0x2A6D6, 1 },
-	{ 0x2F800, 0x2FA1D, 1 },
+	{ 0x0, 0x7F, 5 },	//ASCII
+	{ 0x3000, 0x303F, 3 },	//CJK punctuation
+	{ 0x3040, 0x309F, 3 },	//Japanese hiragana
+	{ 0x30A0, 0x30FF, 3 },	//Japanese katakana
+	{ 0x3100, 0x312F, 3 },	//Chinese Bopomofo
+	{ 0x3400, 0x4DB5, 3 },	//CJK Unified Ideographs Extension A	;Unicode3.0
+	{ 0x4E00, 0x9FA5, 4 },	//CJK Unified Ideographs	;Unicode 1.1
+	{ 0x9FA6, 0x9FBB, 3 },	//CJK Unified Ideographs	;Unicode 4.1
+	{ 0xAC00, 0xD7AF, 3 },	//Korean word
+	{ 0xF900, 0xFA2D, 4 },	//CJK Compatibility Ideographs	;Unicode 1.1
+	{ 0xFA30, 0xFA6A, 4 },	//CJK Compatibility Ideographs	;Unicode 3.2
+	{ 0xFA70, 0xFAD9, 2 },	//CJK Compatibility Ideographs	;Unicode 4.1
+	{ 0xFF00, 0xFFEF, 3},	//Fullwidth ASCII, punctuation, Japanese, Korean
+	{ 0x20000, 0x2A6D6, 1 },//CJK Unified Ideographs Extension B	;Unicode 3.1
+	{ 0x2F800, 0x2FA1D, 1 },//CJK Compatibility Supplement	;Unicode 3.1
 };
 
 void callback(struct bsdconv_instance *ins){
