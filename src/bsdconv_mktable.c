4b797df411568bae0591367c8895de012e19714e
Buganini Q <buganini@gmail.com>
2011-11-30 03:12:55 +0800

    src/bsdconv_mktable.c: improve readability

--- src/bsdconv_mktable.c
+++ src/bsdconv_mktable.c
@@ -32,23 +32,26 @@ extern const char *fmalloc_template;
 #endif
 
 struct m_data_st{
+	/* struct data_st */
 	char *data;
 	size_t len;
 	offset_t next;
 
+	/* extra */
 	offset_t offset;
 	struct m_data_st *n;
 };
 
 struct m_state_st{
+	/* struct state_st */
 	char status;
 	struct m_data_st *data;
 	uint16_t beg;
 	uint16_t end;
 	struct m_state_st **base;
 
-	int prio;
-
+	/* extra */
+	int level;
 	offset_t offset;
 	struct m_state_st *n;
 };
@@ -64,17 +67,19 @@ struct m_state_st * state_new(int status){
 	return ret;
 }
 
-struct list{
-	struct m_state_st *p;
-	int u,l,pr;
-	struct list *n;
+struct states{
+	struct m_state_st *state;
+	int upper_bound;
+	int lower_bound;
+	int level;
+	struct states *n;
 };
 
-struct dhash{
+struct hash{
 	uintptr_t v;
 	int c;
-	struct dhash *p;
-	struct dhash **sub;
+	struct hash *parent;
+	struct hash **sub;
 	offset_t offset;
 	void *head;
 };
@@ -85,19 +90,19 @@ char ci_table[256]={0};
 uintptr_t offset=0;
 struct m_state_st *state_r, *state_t, holder;
 
-struct dhash *hash_datalist, *hash_data;
+struct hash *datalist_hash, *data_hash;
 
 uintptr_t hash(int *p, uintptr_t l){
 	int i;
-	struct dhash *hash_p=hash_datalist;
-	struct dhash *hash_q=hash_data;
+	struct hash *hash_p=datalist_hash;
+	struct hash *hash_q=data_hash;
 	for(i=l-1;i>=0;--i){
 		if(hash_p->sub==NULL)
-			hash_p->sub=calloc(257, sizeof(struct dhash *));
+			hash_p->sub=calloc(257, sizeof(struct hash *));
 		if(hash_p->sub[p[i]]==NULL){
-			hash_p->sub[p[i]]=FMALLOC(sizeof(struct dhash));
+			hash_p->sub[p[i]]=FMALLOC(sizeof(struct hash));
 			hash_p->sub[p[i]]->c=p[i];
-			hash_p->sub[p[i]]->p=hash_p;
+			hash_p->sub[p[i]]->parent=hash_p;
 			hash_p->sub[p[i]]->v=0;
 			hash_p->sub[p[i]]->offset=0;
 			hash_p->sub[p[i]]->sub=NULL;
@@ -106,15 +111,15 @@ uintptr_t hash(int *p, uintptr_t l){
 		if(p[i]==256){
 			hash_p->v=(uintptr_t) hash_q;
 			hash_p=hash_p->sub[p[i]];
-			hash_q=hash_data;
+			hash_q=data_hash;
 		}else{
 			hash_p=hash_p->sub[p[i]];
 			if(hash_q->sub==NULL)
-				hash_q->sub=calloc(257, sizeof(struct dhash *));
+				hash_q->sub=calloc(257, sizeof(struct hash *));
 			if(hash_q->sub[p[i]]==NULL){
-				hash_q->sub[p[i]]=FMALLOC(sizeof(struct dhash));
+				hash_q->sub[p[i]]=FMALLOC(sizeof(struct hash));
 				hash_q->sub[p[i]]->c=p[i];
-				hash_q->sub[p[i]]->p=hash_q;
+				hash_q->sub[p[i]]->parent=hash_q;
 				hash_q->sub[p[i]]->v=0;
 				hash_q->sub[p[i]]->offset=0;
 				hash_q->sub[p[i]]->head=NULL;
@@ -126,20 +131,20 @@ uintptr_t hash(int *p, uintptr_t l){
 	hash_p->v=(uintptr_t) hash_q;
 	return (uintptr_t) hash_p;
 }
-struct dhash *callback_hash;
+struct hash *callback_hash;
 
 uintptr_t hash_callback(int *p, uintptr_t l){
 	int i;
-	struct dhash *hash_p=hash_datalist;
-	struct dhash *hash_q=callback_hash;
+	struct hash *hash_p=datalist_hash;
+	struct hash *hash_q=callback_hash;
 	for(i=l-1;i>=0;--i){
 		hash_p=hash_p->sub[p[i]];
 		if(hash_q->sub==NULL)
-			hash_q->sub=calloc(257, sizeof(struct dhash *));
+			hash_q->sub=calloc(257, sizeof(struct hash *));
 		if(hash_q->sub[p[i]]==NULL){
-			hash_q->sub[p[i]]=FMALLOC(sizeof(struct dhash));
+			hash_q->sub[p[i]]=FMALLOC(sizeof(struct hash));
 			hash_q->sub[p[i]]->c=p[i];
-			hash_q->sub[p[i]]->p=hash_q;
+			hash_q->sub[p[i]]->parent=hash_q;
 			hash_q->sub[p[i]]->v=0;
 			hash_q->sub[p[i]]->offset=0;
 			hash_q->sub[p[i]]->head=NULL;
@@ -162,95 +167,32 @@ uintptr_t hash_callback(int *p, uintptr_t l){
 }
 
 int main(int argc, char *argv[]){
-	int i, j, k, c=0, cu, cl, ci,pr;
+	int i, j, k, c=0, cu, cl, case_insensitive, level;
 	FILE *fp;
 	char inbuf[1024], *f, *t, *tmp, *of, *ot;
 	int dat[1024];
 	offset_t sub[257];
 	uintptr_t l,ret,callback_state=0;
 	struct m_data_st *data_r=NULL, *data_p=NULL, *data_q=NULL, *data_t=NULL;
-	struct list *todo=NULL, *newtodo, *newtodo_tail, *state_p;
+	struct states *todo=NULL, *newtodo, *newtodo_tail, *todo_item;
 	struct state_st dstate;
 	struct data_st ddata;
-	struct dhash *hash_p;
+	struct hash *hash_p;
 	int callback=0;
 	void *tofree;
 	char *bsdconv_mktable_fmalloc_template;
 
-	table['0']=0;
-	table['1']=1;
-	table['2']=2;
-	table['3']=3;
-	table['4']=4;
-	table['5']=5;
-	table['6']=6;
-	table['7']=7;
-	table['8']=8;
-	table['9']=9;
-	table['a']=10;
-	table['A']=10;
-	table['b']=11;
-	table['B']=11;
-	table['c']=12;
-	table['C']=12;
-	table['d']=13;
-	table['D']=13;
-	table['e']=14;
-	table['E']=14;
-	table['f']=15;
-	table['F']=15;
-	ci_table['A']='a';
-	ci_table['B']='b';
-	ci_table['C']='c';
-	ci_table['D']='d';
-	ci_table['E']='e';
-	ci_table['F']='f';
-	ci_table['G']='g';
-	ci_table['H']='h';
-	ci_table['I']='i';
-	ci_table['J']='j';
-	ci_table['K']='k';
-	ci_table['L']='l';
-	ci_table['M']='m';
-	ci_table['N']='n';
-	ci_table['O']='o';
-	ci_table['P']='p';
-	ci_table['Q']='q';
-	ci_table['R']='r';
-	ci_table['S']='s';
-	ci_table['T']='t';
-	ci_table['U']='u';
-	ci_table['V']='v';
-	ci_table['W']='w';
-	ci_table['X']='x';
-	ci_table['Y']='y';
-	ci_table['Z']='z';
-	ci_table['a']='A';
-	ci_table['b']='B';
-	ci_table['c']='C';
-	ci_table['d']='D';
-	ci_table['e']='E';
-	ci_table['f']='F';
-	ci_table['g']='G';
-	ci_table['h']='H';
-	ci_table['i']='I';
-	ci_table['j']='J';
-	ci_table['k']='K';
-	ci_table['l']='L';
-	ci_table['m']='M';
-	ci_table['n']='N';
-	ci_table['o']='O';
-	ci_table['p']='P';
-	ci_table['q']='Q';
-	ci_table['r']='R';
-	ci_table['s']='S';
-	ci_table['t']='T';
-	ci_table['u']='U';
-	ci_table['v']='V';
-	ci_table['w']='W';
-	ci_table['x']='X';
-	ci_table['y']='Y';
-	ci_table['z']='Z';
+	for(i=0;i<10;++i){
+		table['0'+i]=i;
+	}
+	for(i=0;i<6;++i){
+		table['a'+i]=10+i;
+		table['A'+i]=10+i;
+	}
+	for(i=0;i<=26;++i){
+		ci_table['A'+i]='a'+i;
+		ci_table['a']='A'+i;
+	}
 
 	printf("Making table %s\n", argv[1]);
 
@@ -262,31 +204,31 @@ int main(int argc, char *argv[]){
 
 	fp=fopen(argv[1], "r");
 
-	hash_datalist=FMALLOC(sizeof(struct dhash));
-	hash_datalist->sub=NULL;
-	hash_datalist->p=0;
-	hash_datalist->v=0;
-	hash_datalist->offset=0;
-	hash_datalist->head=NULL;
+	datalist_hash=FMALLOC(sizeof(struct hash));
+	datalist_hash->sub=NULL;
+	datalist_hash->parent=0;
+	datalist_hash->v=0;
+	datalist_hash->offset=0;
+	datalist_hash->head=NULL;
 
-	hash_data=FMALLOC(sizeof(struct dhash));
-	hash_data->sub=NULL;
-	hash_data->p=0;
-	hash_data->v=0;
-	hash_data->offset=0;
-	hash_data->head=NULL;
+	data_hash=FMALLOC(sizeof(struct hash));
+	data_hash->sub=NULL;
+	data_hash->parent=0;
+	data_hash->v=0;
+	data_hash->offset=0;
+	data_hash->head=NULL;
 
-	callback_hash=FMALLOC(sizeof(struct dhash));
+	callback_hash=FMALLOC(sizeof(struct hash));
 	callback_hash->sub=NULL;
-	callback_hash->p=0;
+	callback_hash->parent=0;
 	callback_hash->v=0;
 	callback_hash->offset=0;
 	callback_hash->head=NULL;
 
-	newtodo=malloc(sizeof(struct list));
+	newtodo=malloc(sizeof(struct states));
 	newtodo->n=NULL;
-	newtodo->l=3;
-	newtodo->u=2;
+	newtodo->lower_bound=3;
+	newtodo->upper_bound=2;
 	newtodo_tail=newtodo;
 
 	state_t=state_r=state_new(DEADEND);
@@ -309,19 +251,19 @@ int main(int argc, char *argv[]){
 			t=ot=NULL;
 		}
 
-		todo=malloc(sizeof(struct list));
+		todo=malloc(sizeof(struct states));
 		todo->n=NULL;
-		todo->p=&holder;
-		todo->u=0;
-		todo->l=0;
-		todo->pr=0;
+		todo->state=&holder;
+		todo->upper_bound=0;
+		todo->lower_bound=0;
+		todo->level=0;
 
 		//read input sequence, build tree (not including leaves), generate todo list
 		if(*f=='!'){
-			ci=1;
+			case_insensitive=1;
 			++f;
 		}else{
-			ci=0;
+			case_insensitive=0;
 		}
 		while(*f){
 			if(*f=='*'){
@@ -336,53 +278,53 @@ int main(int argc, char *argv[]){
 				cl+=table[(unsigned char)*f];
 				cu=cl;
 			}
-			state_p=todo;
+			todo_item=todo;
 			while(todo){
-				state_p=todo;
+				todo_item=todo;
 				for(c=0;c<=256;++c){
-					if(c>=state_p->l && c<=state_p->u){
-						pr=1;
-					}else if(ci && ci_table[c] && ci_table[c]>=state_p->l && ci_table[c]<=state_p->u){
-						pr=0;
+					if(c>=todo_item->lower_bound && c<=todo_item->upper_bound){
+						level=1;
+					}else if(case_insensitive && ci_table[c] && ci_table[c]>=todo_item->lower_bound && ci_table[c]<=todo_item->upper_bound){
+						level=0;
 					}else{
 						continue;
 					}
-					if(c < state_p->p->beg)
-						state_p->p->beg=c;
-					if(c >= state_p->p->end)
-						state_p->p->end=c+1;
-					if(state_p->p->base==NULL)
-						state_p->p->base=calloc(257, sizeof(struct m_state_st *));
-					if(state_p->p->base[c]){
-						if(state_p->p->base[c]->status==MATCH){
-							state_p->p->base[c]->status=SUBMATCH;
-						}else if(state_p->p->base[c]->status==DUMMY){
-							state_p->p->base[c]=state_t->n=state_new(SUBROUTINE);
+					if(c < todo_item->state->beg)
+						todo_item->state->beg=c;
+					if(c >= todo_item->state->end)
+						todo_item->state->end=c+1;
+					if(todo_item->state->base==NULL)
+						todo_item->state->base=calloc(257, sizeof(struct m_state_st *));
+					if(todo_item->state->base[c]){
+						if(todo_item->state->base[c]->status==MATCH){
+							todo_item->state->base[c]->status=SUBMATCH;
+						}else if(todo_item->state->base[c]->status==DUMMY){
+							todo_item->state->base[c]=state_t->n=state_new(SUBROUTINE);
 							state_t=state_t->n;
 						}
-						newtodo_tail->n=malloc(sizeof(struct list));
+						newtodo_tail->n=malloc(sizeof(struct states));
 						newtodo_tail=newtodo_tail->n;
-						newtodo_tail->p=state_p->p->base[c];
-						newtodo_tail->l=cl;
-						newtodo_tail->u=cu;
-						newtodo_tail->pr=state_p->pr+pr;
+						newtodo_tail->state=todo_item->state->base[c];
+						newtodo_tail->lower_bound=cl;
+						newtodo_tail->upper_bound=cu;
+						newtodo_tail->level=todo_item->level+level;
 						newtodo_tail->n=NULL;
 					}else{
-						//printf("%u[%X]=%u\n", state_p->p, c, offset);
-						state_t->n=state_p->p->base[c]=state_new(CONTINUE);
+						//printf("%u[%X]=%u\n", todo_item->state, c, offset);
+						state_t->n=todo_item->state->base[c]=state_new(CONTINUE);
 						state_t=state_t->n;
 
-						newtodo_tail->n=malloc(sizeof(struct list));
+						newtodo_tail->n=malloc(sizeof(struct states));
 						newtodo_tail=newtodo_tail->n;
 						newtodo_tail->n=NULL;
-						newtodo_tail->p=state_p->p->base[c];
-						newtodo_tail->l=cl;
-						newtodo_tail->u=cu;
-						newtodo_tail->pr=state_p->pr+pr;
+						newtodo_tail->state=todo_item->state->base[c];
+						newtodo_tail->lower_bound=cl;
+						newtodo_tail->upper_bound=cu;
+						newtodo_tail->level=todo_item->level+level;
 					}
 				}
 				todo=todo->n;
-				free(state_p);
+				free(todo_item);
 			}
 			todo=newtodo->n;
 			newtodo->n=NULL;
@@ -407,50 +349,50 @@ int main(int argc, char *argv[]){
 				if(k){
 					k=0;
 					while(todo){
-						state_p=todo;
+						todo_item=todo;
 						for(c=0;c<=256;++c){
-							if(c>=state_p->l && c<=state_p->u){
-								pr=1;
-							}else if(ci && ci_table[c] && ci_table[c]>=state_p->l && ci_table[c]<=state_p->u){
-								pr=0;
+							if(c>=todo_item->lower_bound && c<=todo_item->upper_bound){
+								level=1;
+							}else if(case_insensitive && ci_table[c] && ci_table[c]>=todo_item->lower_bound && ci_table[c]<=todo_item->upper_bound){
+								level=0;
 							}else{
 								continue;
 									}
-							if(c < state_p->p->beg)
-								state_p->p->beg=c;
-							if(c >= state_p->p->end)
-								state_p->p->end=c+1;
-							if(state_p->p->base==NULL)
-								state_p->p->base=calloc(257, sizeof(struct m_state_st *));
-							if(state_p->p->base[c]){
-								if((state_p->p->base[c]->status==MATCH || state_p->p->base[c]->status==SUBMATCH) && (pr+state_p->pr) <= state_p->p->base[c]->prio){
+							if(c < todo_item->state->beg)
+								todo_item->state->beg=c;
+							if(c >= todo_item->state->end)
+								todo_item->state->end=c+1;
+							if(todo_item->state->base==NULL)
+								todo_item->state->base=calloc(257, sizeof(struct m_state_st *));
+							if(todo_item->state->base[c]){
+								if((todo_item->state->base[c]->status==MATCH || todo_item->state->base[c]->status==SUBMATCH) && (level+todo_item->level) <= todo_item->state->base[c]->level){
 //									printf("Duplicated key: %s dropping data: %s\n", of, ot);
 									continue;
 								}else{
-									if(state_p->p->base[c]->status==DUMMY){
-										state_p->p->base[c]=state_t->n=state_new(MATCH);
+									if(todo_item->state->base[c]->status==DUMMY){
+										todo_item->state->base[c]=state_t->n=state_new(MATCH);
 										state_t=state_t->n;
-									}else if(state_p->p->base[c]->status==CONTINUE || state_p->p->base[c]->status==SUBROUTINE){
-										state_p->p->base[c]->status=SUBMATCH;
+									}else if(todo_item->state->base[c]->status==CONTINUE || todo_item->state->base[c]->status==SUBROUTINE){
+										todo_item->state->base[c]->status=SUBMATCH;
 									}else{
-										state_p->p->base[c]->status=MATCH;
+										todo_item->state->base[c]->status=MATCH;
 									}
 								}
 							}else if(callback){
-								state_p->p->base[c]=(struct m_state_st *) callback_state;
+								todo_item->state->base[c]=(struct m_state_st *) callback_state;
 							}else{
-								state_p->p->base[c]=state_t->n=state_new(MATCH);
+								todo_item->state->base[c]=state_t->n=state_new(MATCH);
 								state_t=state_t->n;
 							}
 							if(l){
-								state_p->p->base[c]->data=(struct m_data_st *)ret;
+								todo_item->state->base[c]->data=(struct m_data_st *)ret;
 							}else{
-								state_p->p->base[c]->data=0;
+								todo_item->state->base[c]->data=0;
 							}
-							state_p->p->base[c]->prio=pr+state_p->pr;
+							todo_item->state->base[c]->level=level+todo_item->level;
 						}
 						todo=todo->n;
-						free(state_p);
+						free(todo_item);
 					}
 				}
 				l=0;
@@ -495,12 +437,12 @@ int main(int argc, char *argv[]){
 	state_t=state_r;
 	while(state_t){
 		dstate.status=state_t->status;
-		hash_p=(struct dhash *)state_t->data;
+		hash_p=(struct hash *)state_t->data;
 		k=1;	//begin of data cell
 		l=0;	//length counter
 		j=0;	//has read something to proceed
 		data_q=NULL;	//pointer of last data cell
-		while(hash_p && hash_p->p){
+		while(hash_p && hash_p->parent){
 			if(k){
 				k=0;
 				if(!hash_p->offset){
@@ -543,13 +485,13 @@ int main(int argc, char *argv[]){
 			}else{
 				l+=1;				
 			}
-			hash_p=hash_p->p;
+			hash_p=hash_p->parent;
 		}
 		if(j){
 			j=0;
 			data_p->len=l;
 		}
-		hash_p=(struct dhash *)state_t->data;
+		hash_p=(struct hash *)state_t->data;
 		if(hash_p)
 			dstate.data=(struct data_st *)(uintptr_t)hash_p->offset;
 		else
@@ -592,13 +534,13 @@ int main(int argc, char *argv[]){
 
 	data_t=data_r;
 	while(data_t){
-		hash_p=(struct dhash *)data_t->data;
+		hash_p=(struct hash *)data_t->data;
 		if(hash_p && !hash_p->head){
 			hash_p->head=data_t->data;
-			hash_p=hash_p->p;
-			while(hash_p && hash_p->p){
+			hash_p=hash_p->parent;
+			while(hash_p && hash_p->parent){
 				hash_p->head=data_t->data;
-				hash_p=hash_p->p;
+				hash_p=hash_p->parent;
 			}
 		}
 		data_t=data_t->n;
@@ -606,19 +548,19 @@ int main(int argc, char *argv[]){
 	
 	data_t=data_r;
 	while(data_t){
-		hash_p=(struct dhash *)data_t->data;
+		hash_p=(struct hash *)data_t->data;
 		if(hash_p && !hash_p->offset){
 			fseek(fp, offset, SEEK_SET);
 			hash_p=hash_p->head;
-			while(hash_p && hash_p->p){
+			while(hash_p && hash_p->parent){
 				hash_p->offset=offset;
 				//printf("Writing byte: %X.\n", hash_p->c);
 				fputc(hash_p->c, fp);
 				offset+=1;
-				hash_p=hash_p->p;
+				hash_p=hash_p->parent;
 			}
 		}
-		hash_p=(struct dhash *)data_t->data;
+		hash_p=(struct hash *)data_t->data;
 		if(hash_p)
 			ddata.data=(char *)(uintptr_t)hash_p->offset;
 		else
@@ -626,7 +568,7 @@ int main(int argc, char *argv[]){
 		ddata.len=data_t->len;
 		ddata.next=(struct data_st *)(uintptr_t)data_t->next;
 		fseek(fp, data_t->offset, SEEK_SET);
-		//printf("Writing struct data_st at %p %c %p.\n", (void *)(uintptr_t)data_t->offset, hash_p->p->c, ddata.next);
+		//printf("Writing struct data_st at %p %c %p.\n", (void *)(uintptr_t)data_t->offset, hash_p->parent->c, ddata.next);
 		fwrite((void *)&ddata, sizeof(struct data_st), 1, fp);
 		tofree=data_t;
 		data_t=data_t->n;
