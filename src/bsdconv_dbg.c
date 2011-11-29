a9c495cf45ed84cbec46de6b854f98378ebf937e
Buganini Q <buganini@gmail.com>
2011-11-29 21:37:31 +0800

    bsdconv_dbg: improve data display

--- src/bsdconv_dbg.c
+++ src/bsdconv_dbg.c
@@ -5,6 +5,11 @@
 extern int loadcodec(struct bsdconv_codec_t *cd, int type);
 extern void unloadcodec(struct bsdconv_codec_t *cd);
 
+void print_state(struct state_rt *state);
+void print_data(uintptr_t val);
+
+struct bsdconv_codec_t cd;
+
 void print_state(struct state_rt *state){
 	const char *s=NULL;
 	switch(state->status){
@@ -19,7 +24,25 @@ void print_state(struct state_rt *state){
 		case NOOP: s="NOOP"; break;
 	}
 	printf("State: %s\n", s);
-	printf("Data: %p\n", state->data);
+	printf("Data:");
+	print_data((uintptr_t)state->data);
+}
+
+void print_data(uintptr_t val){
+	struct data_rt data;
+	int i;
+	if(val==0)
+		printf(" 0x0");
+	else while(val){
+		memcpy(&data, (char *)(cd.z+val), sizeof(struct data_st));
+		data.data=(cd.z+(uintptr_t)data.data);
+		for(i=0;i<data.len;++i){
+			printf("%02X", UCP(data.data)[i]);
+		}
+		printf("\n");
+		val=(uintptr_t)data.next;
+	}
+	printf("\n");
 }
 
 int main(int argc, char *argv[]){
@@ -27,7 +50,6 @@ int main(int argc, char *argv[]){
 		return 1;
 
 	int type;
-	struct bsdconv_codec_t cd;
 
 	if(strcmp(argv[1], "FROM")==0){
 		type=FROM;
@@ -44,12 +66,10 @@ int main(int argc, char *argv[]){
 
 
 	struct state_rt state;
-	struct data_rt data;
 	offset_t offset;
 	char op[16];
 	unsigned char c;
 	uintptr_t val;
-	int i;
 	memcpy(&state, cd.z, sizeof(struct state_rt));
 	while(!feof(stdin)){
 		printf("bsdconv_dbg> ");
@@ -63,15 +83,7 @@ int main(int argc, char *argv[]){
 			memcpy(&state, cd.z + offset, sizeof(struct state_st));
 			print_state(&state);
 		}else if(strcmp(op, "data")==0){
-			while(val){
-				memcpy(&data, (char *)(cd.z+val), sizeof(struct data_st));
-				data.data=(cd.z+(uintptr_t)data.data);
-				for(i=0;i<data.len;++i){
-					printf("%02X", UCP(data.data)[i]);
-				}
-				printf("\n");
-				val=(uintptr_t)data.next;
-			}
+			print_data(val);
 		}else if(strcmp(op, "reset")==0){
 			memcpy(&state, cd.z, sizeof(struct state_rt));
 		}else{
