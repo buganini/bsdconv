9885990adf7ceef20143b3977f91fbd6fb1f2384
Buganini Q <buganini@gmail.com>
2012-09-20 09:57:13 +0800

    improve tools/setEnvVar.bat

--- tools/setEnvVar.bat
+++ tools/setEnvVar.bat
@@ -1,2 +1,4 @@
 @echo off
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v BSDCONV_PATH /t REG_SZ /d "%CD%" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v BSDCONV_PATH /t REG_SZ /d %~dp0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /d %PATH%;%~dp0 /f
+pause
