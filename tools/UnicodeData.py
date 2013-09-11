8da062a482af6ff440d9e46240723bc74d38e7a3
Buganini Q <buganini@gmail.com>
2013-09-12 02:19:39 +0800

    cleanup unicodedata generator

--- tools/UnicodeData.py
+++ tools/UnicodeData.py
@@ -2,7 +2,7 @@ import sys
 import urllib
 
 def bsdconv01(dt):
-	dt=dt.lstrip("0")
+	dt=dt.lstrip("0").upper()
 	if len(dt) & 1:
 		return "010"+dt
 	else:
@@ -12,19 +12,19 @@ def bsdconv01(dt):
 f_ccc=open("codecs/inter/_NF_CCC.inc", "w")
 f_nfd=open("codecs/inter/_NFD.txt", "w")
 f_nfkd=open("codecs/inter/_NFKD.txt", "w")
-nfc=open("codecs/inter/_NFC.txt", "w")
-nfkc=open("codecs/inter/_NFKC.txt", "w")
-upper=open("codecs/inter/UPPER.txt", "w")
-lower=open("codecs/inter/LOWER.txt", "w")
+f_nfc=open("codecs/inter/_NFC.txt", "w")
+f_upper=open("codecs/inter/UPPER.txt", "w")
+f_lower=open("codecs/inter/LOWER.txt", "w")
 
 ccc_start=-1
 ccc_end=-1
-ccc_value=-1
+ccc_value=0
+t_ccc={}
 m_nfd={}
 m_nfkd={}
 
 f_ccc.write("/* Generated from {url}*/\n".format(url=sys.argv[1]));
-for f in [nfc, f_nfd, nfkc, f_nfkd, upper, lower]:
+for f in [f_nfc, f_nfd, f_nfkc, f_nfkd, f_upper, f_lower]:
 	f.write("Source: {url}\n".format(url=sys.argv[1]))
 
 def lookup(l,m):
@@ -35,6 +35,30 @@ def lookup(l,m):
 		else:
 			ret.append(e)
 	return ret
+
+def nf_order(l):
+	r=[]
+	b=0
+	e=0
+	for i in range(len(l)):
+		if l[i] in t_ccc:
+			if b==0:
+				b=i
+				e=i
+			else:
+				e=i
+		else:
+			if b!=e:
+				r.append((b,e))
+			b=0
+			e=0
+	if b!=e:
+		r.append((b,e))
+	for b,e in r:
+		a=sorted(l[b:e+1], key=lambda x:t_ccc[x])
+		for i in range(b,e+1):
+			l[i]=a[i-b]
+	return l
 
 l_nfd=[]
 l_nfkd=[]
@@ -47,10 +71,12 @@ for l in ud:
 	code_point=int(a[0], 16)
 	if a[3]!="0":
 		ccc=int(a[3])
+		if ccc:
+			t_ccc[cp]=ccc
 		if ccc==ccc_value and code_point==ccc_end+1:
 			ccc_end=code_point
 		else:
-			if ccc_value!=-1:
+			if ccc_value!=0:
 				f_ccc.write("{0x%x, 0x%x, %d},\n" % (ccc_start, ccc_end, ccc_value))
 			ccc_start=code_point
 			ccc_end=code_point
@@ -73,23 +99,30 @@ for l in ud:
 			m_nfd[cp]=dt
 	if a[12]:
 		dt=bsdconv01(a[12])
-		upper.write("{f}\t{t}\n".format(f=cp, t=dt))
+		f_upper.write("{f}\t{t}\n".format(f=cp, t=dt))
 	if a[13]:
 		dt=bsdconv01(a[13])
-		lower.write("{f}\t{t}\n".format(f=cp, t=dt))
+		f_lower.write("{f}\t{t}\n".format(f=cp, t=dt))
 
 f_ccc.write("{0x%x, 0x%x, %d},\n" % (ccc_start, ccc_end, ccc_value))
 f_ccc.close()
 
+f_upper.close()
+f_lower.close()
+
 for cp in l_nfd:
-	f_nfd.write("{f}\t{t}\n".format(f=cp, t=",".join(lookup(m_nfd[cp], m_nfd))))
+	d=nf_order(lookup(m_nfd[cp], m_nfd))
+	m_nfd[cp]=d
+	f_nfd.write("{f}\t{t}\n".format(f=cp, t=",".join(d)))
 f_nfd.close()
 
 for cp,tag in l_nfkd:
-	f_nfkd.write("{f}\t{t}\t#{c}\n".format(f=cp, t=",".join(lookup(m_nfkd[cp], m_nfd)), c=tag))
+	d=nf_order(lookup(m_nfkd[cp], m_nfkd))
+	m_nfkd[cp]=d
+	f_nfkd.write("{f}\t{t}\t#{c}\n".format(f=cp, t=",".join(d), c=tag))
 f_nfkd.close()
 
-nfc.close()
-nfkc.close()
-upper.close()
-lower.close()
+for cp in l_nfd:
+	l=m_nfd[cp]
+	f_nfc.write("{f}\t{t}\n".format(f=",".join(l), t=cp))
+f_nfc.close()
