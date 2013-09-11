2a0ae76ea1aca9caf74d64a56e748c8353e2b135
Buganini Q <buganini@gmail.com>
2013-09-11 15:50:52 +0800

    generate full decomposition for inter/{_NFD,_NFKD}; update alias for NFD/NFKD

--- tools/UnicodeData.py
+++ tools/UnicodeData.py
@@ -9,26 +9,35 @@ def bsdconv01(dt):
 		return "01"+dt
 
 
-fccc=open("codecs/inter/_NF_CCC.inc", "w")
+f_ccc=open("codecs/inter/_NF_CCC.inc", "w")
+f_nfd=open("codecs/inter/_NFD.txt", "w")
+f_nfkd=open("codecs/inter/_NFKD.txt", "w")
 nfc=open("codecs/inter/_NFC.txt", "w")
-nfd=open("codecs/inter/_NFD.txt", "w")
 nfkc=open("codecs/inter/_NFKC.txt", "w")
-nfkd=open("codecs/inter/_NFKD.txt", "w")
 upper=open("codecs/inter/UPPER.txt", "w")
 lower=open("codecs/inter/LOWER.txt", "w")
 
 ccc_start=-1
 ccc_end=-1
 ccc_value=-1
-nfcm={}
-nfdm={}
-nfkcm={}
-nfkdm={}
+m_nfd={}
+m_nfkd={}
 
-fccc.write("/* Generated from {url}*/\n".format(url=sys.argv[1]));
-for f in [nfc, nfd, nfkc, nfkd, upper, lower]:
+f_ccc.write("/* Generated from {url}*/\n".format(url=sys.argv[1]));
+for f in [nfc, f_nfd, nfkc, f_nfkd, upper, lower]:
 	f.write("Source: {url}\n".format(url=sys.argv[1]))
 
+def lookup(l,m):
+	ret=[]
+	for e in l:
+		if e in m:
+			ret.extend(lookup(m[e], m))
+		else:
+			ret.append(e)
+	return ret
+
+l_nfd=[]
+l_nfkd=[]
 ud=urllib.urlopen(sys.argv[1])
 for l in ud:
 	if not l.strip():
@@ -42,7 +51,7 @@ for l in ud:
 			ccc_end=code_point
 		else:
 			if ccc_value!=-1:
-				fccc.write("{0x%x, 0x%x, %d},\n" % (ccc_start, ccc_end, ccc_value))
+				f_ccc.write("{0x%x, 0x%x, %d},\n" % (ccc_start, ccc_end, ccc_value))
 			ccc_start=code_point
 			ccc_end=code_point
 			ccc_value=ccc
@@ -50,21 +59,18 @@ for l in ud:
 		dt=a[5].split(" ")
 		compat=False
 		if dt[0][0]=="<":
-			mark=dt[0][1:-1]
+			tag=dt[0][1:-1]
 			dt=dt[1:]
 			compat=True
 		dt=[bsdconv01(x) for x in dt]
-		dt=",".join(dt)
 		if compat:
-			nfkd.write("{f}\t{t}\t#{c}\n".format(f=cp, t=dt, c=mark))
-			nfkdm[cp]=dt
-			nfkc.write("{f}\t{t}\t#{c}\n".format(f=dt, t=cp, c=mark))
-			nfkcm[dt]=cp
+			l_nfkd.append((cp,tag))
+			m_nfkd[cp]=dt
 		else:
-			nfd.write("{f}\t{t}\n".format(f=cp, t=dt))
-			nfdm[cp]=dt
-			nfc.write("{f}\t{t}\n".format(f=dt, t=cp))
-			nfcm[dt]=cp
+			l_nfkd.append((cp,"canonical"))
+			m_nfkd[cp]=dt
+			l_nfd.append(cp)
+			m_nfd[cp]=dt
 	if a[12]:
 		dt=bsdconv01(a[12])
 		upper.write("{f}\t{t}\n".format(f=cp, t=dt))
@@ -72,23 +78,18 @@ for l in ud:
 		dt=bsdconv01(a[13])
 		lower.write("{f}\t{t}\n".format(f=cp, t=dt))
 
-fccc.write("{0x%x, 0x%x, %d},\n" % (ccc_start, ccc_end, ccc_value))
-fccc.close()
+f_ccc.write("{0x%x, 0x%x, %d},\n" % (ccc_start, ccc_end, ccc_value))
+f_ccc.close()
+
+for cp in l_nfd:
+	f_nfd.write("{f}\t{t}\n".format(f=cp, t=",".join(lookup(m_nfd[cp], m_nfd))))
+f_nfd.close()
+
+for cp,tag in l_nfkd:
+	f_nfkd.write("{f}\t{t}\t#{c}\n".format(f=cp, t=",".join(lookup(m_nfkd[cp], m_nfd)), c=tag))
+f_nfkd.close()
+
 nfc.close()
-nfd.close()
 nfkc.close()
-nfkd.close()
 upper.close()
 lower.close()
-
-def getMaxDepth(m):
-	return max([getDepth(m, x) for x in m])
-
-def getDepth(m, k):
-	if k in m:
-		return max([getDepth(m, x) for x in m[k].split(",")])+1
-	else:
-		return 0
-
-print "NFC recursion depth:", getMaxDepth(nfcm)
-print "NFKC recursion depth:", getMaxDepth(nfkcm)
