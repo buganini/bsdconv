947e3887e94b1c161a2c955175cce59908120d86
Buganini Q <buganini@gmail.com>
2014-01-28 17:43:02 +0800

    add filter/{KHMER.THAI}

---
+++ modules/filter/KHMER.c
@@ -0,0 +1,10 @@
+#include "../../src/bsdconv.h"
+
+static const struct uint32_range ranges[] = {
+	{ 0x1780, 0x17DD },
+	{ 0x17E0, 0x17E9 },
+	{ 0x17F0, 0x17F9 },
+	{ 0x19E0, 0x19FF },
+};
+
+#include "unicode_range.c"
---
+++ modules/filter/THAI.c
@@ -0,0 +1,8 @@
+#include "../../src/bsdconv.h"
+
+static const struct uint32_range ranges[] = {
+	{ 0xE01, 0xE3A },
+	{ 0xE3F, 0xE5B },
+};
+
+#include "unicode_range.c"
