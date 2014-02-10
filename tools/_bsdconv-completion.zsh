4232b877126e55ec71ac46a585c7573a8f59c63f
Buganini Q <buganini@gmail.com>
2014-02-10 18:56:02 +0800

    handle bsdconv executable's options in zsh completion script

--- tools/_bsdconv-completion.zsh
+++ tools/_bsdconv-completion.zsh
@@ -1,13 +1,28 @@
 #compdef bsdconv
 
 _bsdconv() {
+
 	_arguments \
-		'1: :->conversion'\
+		'1: :->first'\
+		'2: :->second'\
 		'*: :->files'
 
 	case $state in
-	conversion)
-		compadd `bsdconv-completion "$words[2]"`
+	first)
+		if [ "$words[2]" = "-" ]
+		then
+			compadd -- "-l"
+		else
+			compadd `bsdconv-completion "$words[2]"`
+		fi
+	;;
+	second)
+		if [ "$words[3]" = "-" ]
+		then
+			compadd -- "-i"
+		else
+			_files
+		fi
 	;;
 	files)
 		_files
