1b48a6d00d7ca4ca64f44b19633f0676fe441480
Buganini Q <buganini@gmail.com>
2016-08-06 02:59:12 +0800

    add tools/nfkc_gen.py

---
+++ tools/nfkc_gen.py
@@ -0,0 +1,12 @@
+# python nfkc_gen.py '⁰¹²³'|sort|uniq
+import sys
+from bsdconv import Bsdconv
+
+nfkc = Bsdconv("utf-8:nfkc:utf-8")
+i = sys.argv[1].decode("utf-8")
+for c in i:
+	c = c.encode("utf-8")
+	d = nfkc.conv(c)
+	if c==d:
+		continue
+	print("{}\t{}".format(d, c))
